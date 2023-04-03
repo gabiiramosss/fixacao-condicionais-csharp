f1dcf412c8434ef1ca520cfa332e34818b627ef1
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-03 08:30:14 -0300

    Exercício 5 pronto

---
+++ exercício-5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Faça um programa que verifique se uma letra digitada é vogal ou consoante
+
+char letra;
+
+Console.WriteLine($"Digite uma letra: ");
+letra = char.Parse (Console.ReadLine());
+
+if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+{
+    Console.WriteLine($"Vogal");
+}
+else
+{
+    Console.WriteLine($"Consoante");
+}
