d404c85f28263899f186fa369251fbc0f1a6d9cb
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-03 08:40:22 -0300

    Exercício 2 pronto

--- exercício-2/Program.cs
+++ exercício-2/Program.cs
@@ -4,6 +4,9 @@ int numero1;
 int numero2;
 int numero3;
 
+int maior;
+int menor;
+
 Console.WriteLine($"Digite o primeiro número: ");
 numero1 = int.Parse (Console.ReadLine());
 
@@ -15,6 +18,28 @@ numero3 = int.Parse (Console.ReadLine());
 
 if (numero1 > numero2 && numero1 > numero3)
 {
-    
+    maior = numero1;
+}
+else if (numero2 > numero1 && numero2 > numero3)
+{
+    maior = numero2;
+}
+else
+{
+    maior = numero3;
 }
 
+if (numero1 < numero2 && numero1 < numero3)
+{
+    menor = numero1;
+}
+else if (numero2 < numero1 && numero2 < numero3)
+{
+    menor = numero2;
+}
+else
+{
+    menor = numero3;
+}
+
+Console.WriteLine($"O maior número corresponde a {maior} o menor corresponde a {menor}");
